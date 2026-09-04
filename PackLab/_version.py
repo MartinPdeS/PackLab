3f2f3a62e666f03e694b12c5da5234433df4b7d3
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2026-09-04 21:09:36 +0200

    Release v0.7.8

--- PackLab/_version.py
+++ PackLab/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.7.7'
-__version_tuple__ = version_tuple = (0, 7, 7)
+__version__ = version = '0.7.8'
+__version_tuple__ = version_tuple = (0, 7, 8)
 
 __commit_id__ = commit_id = None
